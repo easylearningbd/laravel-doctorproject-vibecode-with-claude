b8e6298ceed770ade4ea6a4668276d739c461409
easylearningbd <elebdsoft@gmail.com>
2026-05-04 04:40:48 +0600

    37 Authentication For Admin Part 3

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -121,7 +121,10 @@
             </div>
             <a class="dropdown-item" href="profile.html">My Profile</a>
             <a class="dropdown-item" href="settings.html">Settings</a>
-            <a class="dropdown-item" href="login.html">Logout</a>
+            <form action="{{ route('agent.logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="dropdown-item">Logout</button>
+            </form>
         </div>
     </li>
     <!-- /User Menu -->
