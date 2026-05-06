6dc8794fdc2e2d9df07373168bdab3c93b8fe2fa
easylearningbd <elebdsoft@gmail.com>
2026-05-07 01:57:22 +0600

    63 Frontend Doctor Details Page Setup Part 2

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -17,12 +17,12 @@
             <li class="d-inline-flex align-items-center drop-header">
                 <div class="dropdown dropdown-country me-3">
                     <a href="javascript:void(0);" class="d-inline-flex align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="assets/img/flags/us-flag.svg" class="me-2" alt="flag">
+                        <img src="{{ asset('backend/assets/img/flags/us-flag.svg') }}" class="me-2" alt="flag">
                     </a>
                     <ul class="dropdown-menu p-2 mt-2">
                         <li>
                             <a class="dropdown-item rounded d-flex align-items-center" href="javascript:void(0);">
-                                <img src="assets/img/flags/us-flag.svg" class="me-2" alt="flag">ENG
+                                <img src="{{ asset('backend/assets/img/flags/us-flag.svg') }}" class="me-2" alt="flag">ENG
                             </a>
                         </li>
                         <li>
@@ -32,7 +32,7 @@
                         </li>
                         <li>
                             <a class="dropdown-item rounded d-flex align-items-center" href="javascript:void(0);">
-                                <img src="assets/img/flags/france-flag.svg" class="me-2" alt="flag">FRA
+                                <img src="{{ asset('backend/assets/img/flags/france-flag.svg') }}" class="me-2" alt="flag">FRA
                             </a>
                         </li>
                     </ul>
@@ -75,14 +75,14 @@
                 </span>
             </a>
             <a href="index.html" class="navbar-brand logo">
-                <img src="assets/img/logo.svg" class="img-fluid" alt="Logo">
+                <img src="{{ asset('backend/assets/img/logo.svg') }}" class="img-fluid" alt="Logo">
             </a>
         </div>
         <div class="header-menu">
             <div class="main-menu-wrapper">
                 <div class="menu-header">
                     <a href="index.html" class="menu-logo">
-                        <img src="assets/img/logo.svg" class="img-fluid" alt="Logo">
+                        <img src="{{ asset('backend/assets/img/logo.svg') }}" class="img-fluid" alt="Logo">
                     </a>
                     <a id="menu_close" class="menu-close" href="javascript:void(0);">
                         <i class="fas fa-times"></i>
@@ -94,156 +94,22 @@
                             
                     </li>
                     <li class="has-submenu">
-                        <a href="javascript:void(0);">Doctors <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="doctor-dashboard.html">Doctor Dashboard</a></li>
-                            <li><a href="appointments.html">Appointments</a></li>
-                            <li><a href="available-timings.html">Available Timing</a></li>
-                            <li><a href="my-patients.html">Patients List</a></li>
-                            <li><a href="patient-profile.html">Patients Profile</a></li>
-                            <li><a href="chat-doctor.html">Chat</a></li>
-                            <li><a href="invoices.html">Invoices</a></li>
-                            <li><a href="doctor-profile-settings.html">Profile Settings</a></li>
-                            <li><a href="reviews.html">Reviews</a></li>
-                            <li><a href="doctor-register.html">Doctor Register</a></li>
-                            <li class="has-submenu">
-                                <a href="doctor-blog.html">Blog</a>
-                                <ul class="submenu">
-                                    <li><a href="doctor-blog.html">Blog</a></li>
-                                    <li><a href="blog-details.html">Blog view</a></li>
-                                    <li><a href="doctor-add-blog.html">Add Blog</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+                        <a href="javascript:void(0);">Doctors   </a>
+                        
                     </li>
                     <li class="has-submenu">
-                        <a href="javascript:void(0);">Patients <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="patient-dashboard.html">Patient Dashboard</a></li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Doctors</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="map-grid.html">Map Grid</a></li>
-                                    <li><a href="map-list.html">Map List</a></li>
-                                    <li><a href="map-list-availability.html">Map with Availability</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Search Doctor</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="search.html">Search Doctor 1</a></li>
-                                    <li><a href="search-2.html">Search Doctor 2</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Doctor Profile</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="doctor-profile.html">Doctor Profile 1</a></li>
-                                    <li><a href="doctor-profile-2.html">Doctor Profile 2</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Booking</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="booking.html">Booking</a></li>
-                                    <li><a href="booking-1.html">Booking 1</a></li>
-                                    <li><a href="booking-2.html">Booking 2</a></li>
-                                    <li><a href="booking-popup.html">Booking Popup</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="checkout.html">Checkout</a></li>
-                            <li><a href="booking-success.html">Booking Success</a></li>
-                            <li><a href="favourites.html">Favourites</a></li>
-                            <li><a href="chat.html">Chat</a></li>
-                            <li><a href="profile-settings.html">Profile Settings</a></li>
-                            <li><a href="change-password.html">Change Password</a></li>
-                        </ul>
+                        <a href="javascript:void(0);">Patients  </a>
+                        
+                    </li>
+                     
+                     
+                    <li class="has-submenu">
+                        <a href="#">Blog  </a>
+                        
                     </li>
                     <li class="has-submenu">
-                        <a href="javascript:void(0);">Pharmacy <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="pharmacy-index.html">Pharmacy</a></li>
-                            <li><a href="pharmacy-details.html">Pharmacy Details</a></li>
-                            <li><a href="pharmacy-search.html">Pharmacy Search</a></li>
-                            <li><a href="product-all.html">Product</a></li>
-                            <li><a href="product-description.html">Product Description</a></li>
-                            <li><a href="cart.html">Cart</a></li>
-                            <li><a href="product-checkout.html">Product Checkout</a></li>
-                            <li><a href="payment-success.html">Payment Success</a></li>
-                            <li><a href="pharmacy-register.html">Pharmacy Register</a></li>
-                        </ul>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="javascript:void(0);">Pages <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="about-us.html">About Us</a></li>
-                            <li><a href="contact-us.html">Contact Us</a></li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Other Pages</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="blank-page.html">Starter Page</a></li>
-                                    <li><a href="pricing.html">Pricing Plan</a></li>
-                                    <li><a href="faq.html">FAQ</a></li>
-                                    <li><a href="maintenance.html">Maintenance</a></li>
-                                    <li><a href="coming-soon.html">Coming Soon</a></li>
-                                    <li><a href="terms-condition.html">Terms & Condition</a></li>
-                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                    <li><a href="components.html">Components</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Authentication</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="login-email.html">Login Email</a></li>
-                                    <li><a href="login-phone.html">Login Phone</a></li>
-                                    <li><a href="doctor-signup.html">Doctor Signup</a></li>
-                                    <li><a href="patient-signup.html">Patient Signup</a></li>
-                                    <li><a href="forgot-password.html">Forgot Password 1</a></li>
-                                    <li><a href="forgot-password2.html">Forgot Password 2</a></li>
-                                    <li><a href="login-email-otp.html">Email OTP</a></li>
-                                    <li><a href="login-phone-otp.html">Phone OTP</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Error Pages</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="error-404.html">404 Error</a></li>
-                                    <li><a href="error-500.html">500 Error</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="hospitals.html">Hospitals</a></li>
-                            <li><a href="speciality.html">Speciality</a></li>
-                            <li><a href="clinic.html">Clinic</a></li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Call</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="voice-call.html">Voice Call</a></li>
-                                    <li><a href="video-call.html">Video Call</a></li>
-                                </ul>
-                            </li>
-                            <li class="has-submenu">
-                                <a href="javascript:void(0);">Invoices</a>
-                                <ul class="submenu inner-submenu">
-                                    <li><a href="invoices.html">Invoices</a></li>
-                                    <li><a href="invoice-view.html">Invoice View</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="#">Blog <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="blog-list.html">Blog List</a></li>
-                            <li><a href="blog-grid.html">Blog Grid</a></li>
-                            <li><a href="blog-details.html">Blog Details</a></li>
-                        </ul>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="#">Admin <i class="fas fa-chevron-down"></i></a>
-                        <ul class="submenu">
-                            <li><a href="admin/index.html" target="_blank">Admin</a></li>
-                            <li><a href="pharmacy/index.html" target="_blank">Pharmacy Admin</a></li>
-                        </ul>
+                        <a href="#">Contact Us  </a>
+                         
                     </li>
                 </ul>
             </div>
